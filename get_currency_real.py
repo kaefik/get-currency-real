54353e1bb3ecf3447fa444722177e912f93bbc34
ILnur Saifutdinov <ilnursoft@gmail.com>
2020-10-29 15:17:46 +0300

    добавил выгрузку курса  евро и поменял вид отправляемого сообщения в телеграмм

--- get_currency_real.py
+++ get_currency_real.py
@@ -10,6 +10,7 @@ https://www.alphavantage.co/query?function=FX_DAILY&from_symbol=EUR&to_symbol=US
 
 import os
 import json
+import time
 import requests
 from dotenv import load_dotenv  # pip3 install python-dotenv
 
@@ -29,19 +30,20 @@ app_api_hash = os.getenv("TLG_APP_API_HASH")
 app_name = os.getenv("TLG_APP_NAME")
 bot_token = os.getenv("I_BOT_TOKEN")
 api_key = os.getenv("API_KEY")
+
+
 # END получение данных окружения
 
 
 def currency_exchange_rate(currency_from, currency_to):
-
-    #currency_from = "USD"
-    #currency_to = "RUB"
+    # currency_from = "USD"
+    # currency_to = "RUB"
     filename = f"currency_exchange_rate_{currency_from}_{currency_to}.json"
 
     urls = f"https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency={currency_from}&" \
-        f"to_currency={currency_to}&apikey={api_key}"
+           f"to_currency={currency_to}&apikey={api_key}"
 
-    session = requests.Session()    
+    session = requests.Session()
     r = session.get(urls)
     # print(r.status_code)
 
@@ -51,7 +53,6 @@ def currency_exchange_rate(currency_from, currency_to):
 
         data_currency_now = r.json()
 
-
     data_currency_last = None
 
     try:
@@ -60,7 +61,7 @@ def currency_exchange_rate(currency_from, currency_to):
     except FileNotFoundError:
         print(f"Нет файла {filename}. Возможно это первый запуск программы.")
 
-    #print(data_currency_last)
+    # print(data_currency_last)
 
     # TEST string
     """
@@ -84,33 +85,33 @@ def currency_exchange_rate(currency_from, currency_to):
 
     # save json into file
     json.dump(data_currency_now, open(filename, "w"))
-    #print(data_currency_now)
+    # print(data_currency_now)
 
     data_curr = data_currency_now["Realtime Currency Exchange Rate"]
 
     msg_txt = f"{data_curr['1. From_Currency Code']}->{data_curr['3. To_Currency Code']}\n" \
-        f"Exchage Rate: {data_curr['5. Exchange Rate']}\n" \
-        f"Time: {data_curr['6. Last Refreshed']} {data_curr['7. Time Zone']}"
+              f"Exchage Rate: {data_curr['5. Exchange Rate']}\n" \
+              f"Time: {data_curr['6. Last Refreshed']} {data_curr['7. Time Zone']}"
 
     return msg_txt
 
-def fx_intraday(currency_from, currency_to, interval = "15min", outputsize="compact"):
 
+def fx_intraday(currency_from, currency_to, interval="15min", outputsize="compact"):
     filename = f"last_{currency_from}_{currency_to}.json"
 
     urls = f"https://www.alphavantage.co/query?function=FX_INTRADAY&from_symbol={currency_from}&" \
-        f"to_symbol={currency_to}&interval={interval}&outputsize={outputsize}&apikey={api_key}"
+           f"to_symbol={currency_to}&interval={interval}&outputsize={outputsize}&apikey={api_key}"
 
-    session = requests.Session()    
+    session = requests.Session()
     r = session.get(urls)
     # print(r.status_code)
 
     if (r.status_code == 200):
         # print(r.headers)
-        #print(r.content)
+        # print(r.content)
 
         data_currency_now = r.json()
-    
+
     data_currency_last = None
 
     try:
@@ -119,12 +120,11 @@ def fx_intraday(currency_from, currency_to, interval = "15min", outputsize="comp
     except FileNotFoundError:
         print(f"Нет файла {filename}. Возможно это первый запуск программы.")
 
-    #print(data_currency_last)
+    # print(data_currency_last)
 
     # TEST string
-    #data_currency_now = data_currency_last
-    #print(data_currency_now)
-    
+    # data_currency_now = data_currency_last
+    # print(data_currency_now)
 
     if data_currency_last is not None:
         # сравнение текущей цены с последней сохраненной
@@ -132,11 +132,10 @@ def fx_intraday(currency_from, currency_to, interval = "15min", outputsize="comp
 
     # save json into file
     json.dump(data_currency_now, open(filename, "w"))
-    #print(data_currency_now)
+    # print(data_currency_now)
 
     msg_txt = "None"
 
-    
     data_curr_meta = data_currency_now["Meta Data"]
     data_curr_timeseries = data_currency_now["Time Series FX (15min)"]
 
@@ -144,21 +143,26 @@ def fx_intraday(currency_from, currency_to, interval = "15min", outputsize="comp
 
     df_curr_timeseries = df_curr_timeseries.T
 
-    #print(df_curr_timeseries.head())
+    # print(df_curr_timeseries.head())
     print()
     last_curr = df_curr_timeseries.iloc[0]
 
     msg_txt = f'{data_curr_meta["1. Information"]}\n{data_curr_meta["2. From Symbol"]}->{data_curr_meta["3. To Symbol"]}\n' \
-        f'open: {last_curr[0]}\nhigh: {last_curr[1]}\nlow: {last_curr[2]}\nclose: {last_curr[3]}\nTime: {last_curr.name}\n'
+              f'open: {last_curr[0]}\nhigh: {last_curr[1]}\nlow: {last_curr[2]}\nclose: {last_curr[3]}\nTime: {last_curr.name}\n'
 
     return msg_txt
 
-if __name__ =="__main__":
+
+if __name__ == "__main__":
     tlg = SendNotify()
-    
-    msg1 = currency_exchange_rate(currency_from="USD", currency_to="RUB")
-    msg2 = fx_intraday(currency_from="USD", currency_to="RUB", interval = "15min", outputsize="compact")
-    msg3 = currency_exchange_rate(currency_from="BTC", currency_to="USD")
-    msg = f"{msg1}\n\n{msg2}\n{msg3}"
-    print(msg)
-    tlg.send_msg(msg)
+
+    msg_usd = currency_exchange_rate(currency_from="USD", currency_to="RUB")
+    time.sleep(5)
+    # msg2 = '' # fx_intraday(currency_from="USD", currency_to="RUB", interval = "15min", outputsize="compact")
+    msg_btc = currency_exchange_rate(currency_from="BTC", currency_to="USD")
+    time.sleep(5)
+    msg_eur = currency_exchange_rate(currency_from="EUR", currency_to="RUB")
+
+    tlg.send_msg(msg_usd + "\n#usd")
+    tlg.send_msg(msg_eur + "\n#eur")
+    tlg.send_msg(msg_btc + "\n#btc")
